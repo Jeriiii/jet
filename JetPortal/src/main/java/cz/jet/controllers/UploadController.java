ec5f6dd491fca3b12337663d60b9e0143d73bd67
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-03-17 19:08:20 +0100

    Formulář na zkoušku.

---
+++ JetPortal/src/main/java/cz/jet/controllers/UploadController.java
@@ -0,0 +1,54 @@
+package cz.jet.controllers;
+
+import cz.jet.models.Subscriber;
+import cz.jet.models.Subscriber.Frequency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+/**
+ *
+ * @author Petr Kukrál
+ */
+ 
+@Controller//anotace kontroleru
+public class UploadController {//nazev tridy musi byt stejny jako url!!!
+    
+    @Autowired 
+    private ApplicationContext context;
+    //////////////////////////////
+	
+	@ModelAttribute("frequencies")
+	public Frequency[] frequencies() {
+		return Frequency.values();
+	}
+
+	@RequestMapping(value="form", method=RequestMethod.GET)
+	public String loadFormPage(Model m) {
+		m.addAttribute("subscriber", new Subscriber());
+		return "upload/form";
+	}
+
+	@RequestMapping(value="form", method=RequestMethod.POST)
+	public String submitForm(Subscriber subscriber, Model m) {
+		m.addAttribute("message", "Successfully saved person: " + subscriber.toString());
+		return "upload/form";
+	}
+	
+	//////////////////////////////
+    /*
+	Priklad fungovani frameworku
+    podle tutorialu http://www.tutorialspoint.com/spring/spring_web_mvc_framework.htm
+    */
+    // url (value=...)se nemusi psat pokazde, lze jej napsat i primo pro celou tridu, viz. tutorial 
+//   @RequestMapping(value = "/upload/form", method = RequestMethod.GET) 
+//   public String printUpload(ModelMap model) {//promenne do viewu
+//      model.addAttribute("message", "Ahoj, tady se nahrávají soubory.");//promenna message predana do jspcka
+//      return "upload/form";// jmeno jspcka, v tomhle pripade se bude hledat hello.jsp (sestavuje se ve view Resolveru - viz dispatcher-servlet.xml)
+//   }
+    
+}
