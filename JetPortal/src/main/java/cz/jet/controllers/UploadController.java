e3aa0def04476fffb19bc1d52d08af3d1a98fe34
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-04-06 18:40:24 +0200

    refactoring

--- JetPortal/src/main/java/cz/jet/controllers/UploadController.java
+++ JetPortal/src/main/java/cz/jet/controllers/UploadController.java
@@ -45,23 +45,17 @@ public class UploadController {
 	
 	@Autowired
     private PomItemsService pomItemsService;
-     
-    //////////////////////////////
 	
-//	@RequestMapping("/fileUploadForm")
-//	public ModelAndView getUploadForm(
-//			@ModelAttribute("uploadedFile") UploadedFile uploadedFile,
-//			BindingResult result) {
-//		return new ModelAndView("uploadForm");
-//	}
+	/**
+	 * render for jsp with formulářem
+	 */
 	
-	// render pro jsp s formulářem
 	@RequestMapping(value="form-upload-file", method=RequestMethod.GET)
 	public String loadFormPage(Model m) {
 		m.addAttribute("uploadedFile", new UploadedFile());              
 		return "upload/formUploadFile";
 	}
-        
+    
 	@RequestMapping(value="form-upload-file", method=RequestMethod.POST)
 	public String fileUploaded(@RequestParam("email") String email,Model m,
 			UploadedFile uploadedFile, BindingResult result) throws InterruptedException {
@@ -75,7 +69,6 @@ public class UploadController {
 		}
 		
 		// include item to database
-		
 		long id = pomItemsService.insertNewPomItem(email);
 		String fileName = Long.toString(id) + ".xml";
 		
@@ -96,85 +89,5 @@ public class UploadController {
 		}
 		return "upload/formUploadFile";
 	}
-    
-//	@RequestMapping(value="form-upload-file", method=RequestMethod.POST)
-//	public ModelAndView fileUploaded(
-//			UploadedFile uploadedFile, BindingResult result) {
-//		InputStream inputStream = null;
-//		OutputStream outputStream = null;
-//
-//		MultipartFile file = uploadedFile.getFile();
-////		fileValidator.validate(uploadedFile, result);
-//
-//		String fileName = file.getOriginalFilename();
-//
-//		if (result.hasErrors()) {
-//			return new ModelAndView("uploadForm");
-//		}
-//
-//		try {
-//			inputStream = file.getInputStream();
-//
-//			// cesta kam se ma soubor ulozit
-//			String path = "C:/webhostJava/files/";
-//			File newFile = new File(path + fileName);
-//			if (!newFile.exists()) {
-//				newFile.createNewFile();
-//			}
-//			outputStream = new FileOutputStream(newFile);
-//			int read = 0;
-//			byte[] bytes = new byte[1024];
-//
-//			while ((read = inputStream.read(bytes)) != -1) {
-//				outputStream.write(bytes, 0, read);
-//			}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block  
-//			e.printStackTrace();
-//		}
-//
-//		return new ModelAndView("upload/formUploadFile", "message", fileName);
-//	}
-	
-//	// spustí se ještě před metodou loadFormPage
-//	// přidá se k modelu pro kontajner na selectbox Frequency
-//	// alternativně by to šlo přepsat rovnou do metody loadFormPage takto: m.addAttribute("frequencies", Frequency.values())
-//	@ModelAttribute("frequencies")
-//	public Frequency[] frequencies() {
-//		return Frequency.values();
-//	}
-//
-//	// render pro jsp s formulářem
-//	@RequestMapping(value="form", method=RequestMethod.GET)
-//	public String loadFormPage(Model m) {
-//		// objekt, do kterého se vloží data z formuláře
-//		Subscriber subscriber = new Subscriber();
-//		// defaultní nastavení
-//		subscriber.setReceiveNewsletter(true);
-//		subscriber.setNewsletterFrequency(Frequency.HOURLY);
-//		
-//		m.addAttribute("subscriber", new Subscriber());
-//		return "upload/form";
-//	}
-//
-//	@RequestMapping(value="form", method=RequestMethod.POST)
-//	public String submitForm(Subscriber subscriber, Model m) throws UnsupportedEncodingException {
-//		
-//		String file = "";//subscriber.getFile().getOriginalFilename();
-//		m.addAttribute("message", "Successfully saved person: " + subscriber.toString() + file);
-//		return "upload/form";
-//	}
-	
-	//////////////////////////////
-    /*
-	Priklad fungovani frameworku
-    podle tutorialu http://www.tutorialspoint.com/spring/spring_web_mvc_framework.htm
-    */
-    // url (value=...)se nemusi psat pokazde, lze jej napsat i primo pro celou tridu, viz. tutorial 
-//   @RequestMapping(value = "/upload/form", method = RequestMethod.GET) 
-//   public String printUpload(ModelMap model) {//promenne do viewu
-//      model.addAttribute("message", "Ahoj, tady se nahrávají soubory.");//promenna message predana do jspcka
-//      return "upload/form";// jmeno jspcka, v tomhle pripade se bude hledat hello.jsp (sestavuje se ve view Resolveru - viz dispatcher-servlet.xml)
-//   }
     
 }
