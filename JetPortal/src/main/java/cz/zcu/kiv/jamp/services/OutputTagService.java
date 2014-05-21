ce99acbb6a6d75b4bcc96842a809069cb0b1a6b4
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-05-21 12:12:52 +0200

    změna vykreslování

--- JetPortal/src/main/java/cz/zcu/kiv/jamp/services/OutputTagService.java
+++ JetPortal/src/main/java/cz/zcu/kiv/jamp/services/OutputTagService.java
@@ -42,13 +42,13 @@ public class OutputTagService {
 		String warning = "[WARNING]";
 
 		if (line.contains(info)) {
-			line = line.replace(info, "<span class='info'>" + info + "</span>");
+			line = line.replace(info, "<span class='label label-info'>" + info + "</span>");
 			//line = "<tr class='info'><td>" + line + "</td></tr>";
 		} else if (line.contains(error)) {
-			line = line.replace(error, "<span class='error'>" + error + "</span>");
+			line = line.replace(error, "<span class='label label-danger'>" + error + "</span>");
 			//line = "<tr class='danger'><td>" + line + "</td></tr>";
 		} else if (line.contains(warning)) {
-			line = line.replace(warning, "<span class='warning'>" + warning + "</span>");
+			line = line.replace(warning, "<span class='label label-warning'>" + warning + "</span>");
 			//line = "<tr class='warning'><td>" + line + "</td></tr>";
 		} else {
 			//line = "<tr><td>" + line + "</td></tr>";
